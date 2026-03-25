7e681c837d9d1d5987c19c2a5e3f110f0b82c98d
oliveai2026 <oliveai2026@users.noreply.github.com>
2026-03-25 11:47:11 +0800

    fix: remove type:module, revert next.config.js to CJS (postcss compat)

--- image-background-remover/next.config.js
+++ image-background-remover/next.config.js
@@ -1,10 +1,4 @@
-import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-if (process.env.NODE_ENV === 'development') {
-  initOpenNextCloudflareForDev()
-}
-
-export default nextConfig
+module.exports = nextConfig
